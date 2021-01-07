b2c6ba13fad71b4d96216b938b0db29fafa730dd
Blair Reynolds <blairreynolds4@gmail.com>
2021-01-07 00:46:34 -0600

    Added more styles for header

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -44,6 +44,8 @@ function Banner() {
 
             <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
             </div>
+
+            <div className="banner_fadeBottom" />
         </header>
     )
 }
